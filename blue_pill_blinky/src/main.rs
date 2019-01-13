6dcc624b59649a8dafc6b5e98cd102ffc5fcefc2
Fabien Tregan <fabien@treXXgan.fr>
2019-01-13 17:57:15 +0100

    Tries to read the system timer but always gets O.

--- blue_pill_blinky/src/main.rs
+++ blue_pill_blinky/src/main.rs
@@ -8,9 +8,31 @@ extern crate panic_halt;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
+#[repr(C)]
+struct SysTick {
+    pub csr: u32,
+    pub rvr: u32,
+    pub cvr: u32,
+    pub calib: u32,
+}
+
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
 
-    loop {}
+    let systick = unsafe { &mut *(0xE000_E010 as *mut SysTick) };
+
+    // Reload  Value Register set to 0x00FFFFFF
+    // when timer starts or reachs 0, set automatically set is back to this value
+    unsafe { core::ptr::write_volatile(&mut systick.rvr, 0x00FFFFFF) };
+    
+    // Timer Control Register set so:
+    // -Timer uses processor clock
+    // -No exception is raised when value reaches zero
+    // -Counter is enabled
+    unsafe { core::ptr::write_volatile(&mut systick.cvr, 0b000000000000000_0_0000000000000_101) };
+
+    loop {
+        let current_value_register = unsafe { core::ptr::read_volatile(&mut systick.cvr) };
+        hprintln!("System timer current value is now {}.", current_value_register).unwrap();
+    }
 }
